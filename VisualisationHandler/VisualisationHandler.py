0e2a867ee6bad6e4003db704ba362e1181b022df
GitiHubi <marco.schreyer@unisg.ch>
2023-01-17 18:04:15 +0100

    add interactive visualization

--- VisualisationHandler/VisualisationHandler.py
+++ VisualisationHandler/VisualisationHandler.py
@@ -2,6 +2,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 import os
 
 mpl.rcParams['agg.path.chunksize'] = 1000000
@@ -106,6 +107,46 @@ class VisualizationHandler(object):
 
         # save plot to plotting directory
         plt.savefig(os.path.join(self.plot_dir, filename), dpi=300)
+
+        # close plot
+        plt.close()
+
+        # plot the learned embedding in the 2D latent space
+    def plot_embeddings_2d_interactive(self, data, z1_col_name, z2_col_name, c_col_name, filename, title):
+
+        # set plotting appearance
+        plt.style.use('seaborn')
+        # plt.rcParams['figure.figsize'] = [9, 11]  # width * height
+        plt.rcParams['figure.figsize'] = [6, 6]  # width * height
+        plt.rcParams['agg.path.chunksize'] = 1000000
+        cm = plt.cm.get_cmap('RdYlBu')
+
+        # determine mean and std of reconstruction error
+        mean = data[c_col_name].mean()
+        std = data[c_col_name].std()
+
+        # scatter plot of embeddings
+        fig = px.scatter(data
+                  , x=z1_col_name
+                  , y=z2_col_name
+                  , color=c_col_name
+                  , labels={z1_col_name : '<b>[z1]</b>', z2_col_name : '<b>[z2]</b>', c_col_name : '<b>Rec. Error</b>'}
+                  , hover_data=['Y_JE_LINE_ITEMS', 'Y_PREPARER_ID', 'Y_SOURCE', 'Y_ACCOUNT_TYPE', 'Y_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NAME']
+                  , title=title)
+
+        # update axis layout
+        fig.update_layout(
+            title=dict(font_size=28, x=0.5, xanchor='center')
+            , hoverlabel=dict(bgcolor="white", font_size=14)
+            , xaxis=dict(titlefont=dict(size=26), tickfont=dict(size=18))
+            , yaxis=dict(titlefont=dict(size=26), tickfont=dict(size=18))
+            )
+
+        # update coloraxis layout
+        fig.update_coloraxes(colorbar_tickfont_size=18, cmin=mean-0.1*std, cmax=mean+0.1*std)
+
+        # save plot to plotting directory
+        fig.write_html(os.path.join(self.plot_dir, filename))
 
         # close plot
         plt.close()
