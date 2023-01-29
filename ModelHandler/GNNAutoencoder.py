76aaf4c7d30ef5c5949754d203691dfc5f351f64
GitiHubi <marco.schreyer@unisg.ch>
2023-01-30 00:41:29 +0100

    various updates

--- ModelHandler/GNNAutoencoder.py
+++ ModelHandler/GNNAutoencoder.py
@@ -10,35 +10,25 @@ import ModelHandler.GNNDecoder as GNNDecoder
 class GNNAutoencoder(nn.Module):
 
     # define class constructor
-    def __init__(self, no_accounts, no_features, hidden_dim, embed_dim, output_dim, device='cpu'):
+    def __init__(self, encoder_dim, bottleneck, decoder_dim, bias=True, device='cpu'):
 
         # call super class constructor
         super(GNNAutoencoder, self).__init__()
 
-        # set global account dimension
-        self.no_accounts = no_accounts
-
-        # set global feature dimension
-        self.no_features = no_features
-
         # init graph VAE encoder model
-        self.encoder = GNNEncoder.GNNEncoder(self.no_features, hidden_dim, embed_dim)
+        self.encoder = GNNEncoder.GNNEncoder(encoder_dim, bottleneck, bias)
 
         # init graph VAE decoder model
-        self.decoder = GNNDecoder.GNNDecoder(hidden_dim, embed_dim, output_dim)
+        self.decoder = GNNDecoder.GNNDecoder(decoder_dim, bias)
 
         # init embedding dimension
         self.device = device
 
     # define graph VAE forward pass
-    def forward(self, features, adj_matrices):
-
-        # init recon features and adjacency matrices
-        recon_features = torch.zeros_like(features)
-        recon_adj_matrices = torch.zeros_like(adj_matrices)
+    def forward(self, feat_matrices, adj_matrices):
 
         # run encoder forward pass
-        z, mu, sigma = self.encoder(features, adj_matrices)
+        z, mu, sigma = self.encoder(feat_matrices, adj_matrices)
 
         # compute sigma sample
         # sigma_sample = sigma.mul(0.5).exp_()
@@ -50,37 +40,67 @@ class GNNAutoencoder(nn.Module):
         # z = eps * sigma_sample + mu
 
         # run decoder forward pass
-        reconstructions = self.decoder(z)
+        rec_feat_matrices, rec_adj_matrices = self.decoder(z)
 
-        # iterate over reconstructions
-        for i, reconstruction in enumerate(reconstructions):
+        # reformat reconstructed feature matrices
+        rec_feat_matrices = rec_feat_matrices.reshape_as(feat_matrices)
 
-            # reconstruct upper triangular matrix from output vector
-            recon_adj_lower = self.reconstruct_adj_upper(output_vector=reconstruction)
-
-            # reconstruct full adjacency matrix from triangular matrix
-            recon_adj_matrix = self.recover_full_adj_from_lower(recon_adj_lower)
-
-            # collect reconstructed adjacency matrices
-            recon_adj_matrices[i, :, :] = recon_adj_matrix
-
-            # determine reconstructed features
-            #recon_feature = torch.sum(recon_adj_matrix, axis=1)
-
-            # collect reconstructed features matrices
-            #recon_features[i, :] = recon_feature
+        # reformat reconstructed adjacency matrices
+        rec_adj_matrices = rec_adj_matrices.reshape_as(adj_matrices)
 
         # return reconstructed features and adjacency matrix
-        return z, mu, sigma, recon_features, recon_adj_matrices
+        return z, mu, sigma, rec_feat_matrices, rec_adj_matrices
+
+    # reformat feat matrices
+    def reconstruct_feat_matrices(self, feat_matrices, rec_feat_matrices, xdim, ydim):
+
+        # init recon features and adjacency matrices
+        rec_feat_matrices_formatted = torch.zeros_like(feat_matrices)
+
+        # iterate over reconstructions
+        for i, rec_feat_matrix in enumerate(rec_feat_matrices):
+
+            # reconstruct upper triangular matrix from output vector
+            rec_feat_lower = self.recover_upper(rec_feat_matrix, xdim, ydim)
+
+            # reconstruct full adjacency matrix from triangular matrix
+            rec_feat_matrix = self.recover_full(rec_feat_lower)
+
+            # collect reconstructed adjacency matrices
+            rec_feat_matrices_formatted[i, :, :] = rec_feat_matrix
+
+        # return reformated adjacency matrix
+        return rec_feat_matrices_formatted
+
+    # reformat adjacency matrices
+    def reconstruct_adj_matrices(self, adj_matrices, rec_adj_matrices, xdim, ydim):
+
+        # init recon adjacency matrices
+        rec_adj_matrices_formatted = torch.zeros_like(adj_matrices)
+
+        # iterate over reconstructions
+        for i, rec_adj_matrix in enumerate(rec_adj_matrices):
+
+            # reconstruct upper triangular matrix from output vector
+            rec_adj_lower = self.recover_upper(rec_adj_matrix, xdim, ydim)
+
+            # reconstruct full adjacency matrix from triangular matrix
+            rec_adj_matrix = self.recover_full(rec_adj_lower)
+
+            # collect reconstructed adjacency matrices
+            rec_adj_matrices_formatted[i, :, :] = rec_adj_matrix
+
+        # return reformated adjacency matrix
+        return rec_adj_matrices_formatted
 
     # reconstruct upper triangular matrix
-    def reconstruct_adj_upper(self, output_vector):
+    def recover_upper(self, output_vector, xdim, ydim):
 
         # init new max-node adjacency matrix
-        adj_matrix = torch.zeros(self.no_accounts, self.no_accounts).double().to(self.device)
+        adj_matrix = torch.zeros(xdim, ydim).double().to(self.device)
 
         # init new upper triangular boolean matrix
-        adj_matrix_upper_triangular = torch.triu(torch.ones(self.no_accounts, self.no_accounts)) == 1
+        adj_matrix_upper_triangular = torch.triu(torch.ones(xdim, ydim)) == 1
 
         # fill new max-node adjacency matrix
         adj_matrix[adj_matrix_upper_triangular] = output_vector
@@ -89,7 +109,7 @@ class GNNAutoencoder(nn.Module):
         return adj_matrix
 
     # reconstruct full triangular matrix
-    def recover_full_adj_from_lower(self, matrix):
+    def recover_full(self, matrix):
 
         # determine matrix diagonal entries
         adj_diagonal = torch.diag(torch.diag(matrix, 0))
