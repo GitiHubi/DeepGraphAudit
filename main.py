75cb3da897f94875b29dfe78ceb82e5ec25968b4
GitiHubi <marco.schreyer@unisg.ch>
2023-01-29 01:18:28 +0100

    towards feature learning

--- main.py
+++ main.py
@@ -37,23 +37,36 @@ def main():
     parser.add_argument('-exp_postfix', type=str, default='poc', help='postfix of experimental runs.')
 
     # data parameter
-    parser.add_argument('-dataset', help='', nargs='?', type=str,  default='ernstyoung')
-    parser.add_argument('-sample_eval', help='', nargs='?', type=str, default='False')
-    parser.add_argument('-sample_size', help='', nargs='?', type=int, default=10000)
+    parser.add_argument('-dataset', help='', nargs='?', type=str,  default='ey') # ey, serpro, sap
+    parser.add_argument('-sample_eval', help='', nargs='?', type=str, default='True')
+    parser.add_argument('-sample_size', help='', nargs='?', type=int, default=1001)
+    parser.add_argument('-min_line_items', help='', nargs='?', type=int, default=2)
+    parser.add_argument('-max_line_items', help='', nargs='?', type=int, default=40)
 
     # model architecture parameter
     parser.add_argument('-seed', type=int, default=1111, help='seed value for deterministic results.')
     parser.add_argument('-hidden_dim', type=int, default=64, help='the dimension of the first gnn layer.')
     parser.add_argument('-embed_dim', type=int, default=2, help='the dimension of the graph embeddings.')
-    parser.add_argument('-kl_div_alpha', type=float, default=0.0, help='the kl-divergence loss regularizer.')
 
     # model training parameter
-    parser.add_argument('-iterations', type=int, default=101, help='the number of training iterations.')
-    parser.add_argument('-eval_iteration', type=int, default=10, help='the eval training iteration.')
+    parser.add_argument('-iterations', type=int, default=1001, help='the number of training iterations.')
+    parser.add_argument('-eval_iteration', type=int, default=100, help='the eval training iteration.')
     parser.add_argument('-batch_size', type=int, default=128, help='the batch size.')
+    parser.add_argument('-loss', type=str, default='mse', help='the training and validation loss.')
     parser.add_argument('-learning_rate', type=float, default=0.0001, help='the learning rate.')
+    parser.add_argument('-kl_div_alpha', type=float, default=0.0, help='the kl-divergence loss regularizer.')
     parser.add_argument('-device', type=str, default='cpu', help='the compute device.')
     parser.add_argument('-wandb', type=str, default='True', help='enable wandb logging.')
+
+    # model anomaly detection paramter
+    parser.add_argument('-algo', type=str, default='hdbscan', help='the anomaly detection algorithm.') # lof, svm, iforest, hdbscan
+    parser.add_argument('-min_cluster_size', type=int, default=100, help='the hdbscan min cluster size.')
+    parser.add_argument('-min_samples', type=int, default=1, help='the hdbscan min samples.')
+    parser.add_argument('-kernel', type=str, default='rbf', help='the one-class svm kernel.')
+    parser.add_argument('-degree', type=int, default=3, help='the one-class svm degree of the polynomial kernel function.')
+    parser.add_argument('-gamma', type=str, default='svm', help='the one-class svm kernel coefficient.')
+    parser.add_argument('-n_neighbors', type=int, default=6, help='the number of LOF neighbors.')
+    parser.add_argument('-leaf_size', type=int, default=30, help='the leaf size of LOF tree creation.')
 
     # ignore potential warnings - mostly due to package updates
     warnings.filterwarnings('ignore')
