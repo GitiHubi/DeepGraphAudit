d8968a9bfe41ec75699b376c431045a5eef0d539
GitiHubi <marco.schreyer@unisg.ch>
2023-02-20 01:50:18 +0100

    various updates

--- start_ey_grid_search.sh
+++ start_ey_grid_search.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
 
 # set directory parameter
-run_dir=sc_ey_004
+run_dir=sc_ey_001
 base_dir=./200_experiments/$run_dir
 data_dir=./100_datasets
 log_dir=./300_logs/$run_dir
-
-# set seed parameter
-declare -a seeds=(1111 2222 3333 4444 1234)
-
-# set alpha parameter
-declare -a betas=(0.0 0.1)
-# declare -a betas=(0.2)
 
 # set data preparation parameter
 dataset=ey
 sample_eval=False
 sample_size=2001
 
+# set seed parameter
+declare -a seeds=(1111 2222 3333 4444 1234)
+
+# set alpha parameter
+declare -a betas=(0.0 0.1 0.5)
+
 # set architecture parameter
-declare -a encoder_dim=(128 64 32 16 8 4 2)
-declare -a decoder_dim=(2 4 8 16 32 64 128)
-# declare -a encoder_dim=(8 4 2)
-# declare -a decoder_dim=(2 4 8)
-declare -a bottlenecks=(linear tanh)
-embed_dim=2
+declare -a encoder_dim=(32 8 4 2)
+declare -a decoder_dim=(2 4 8 32)
+declare -a feat_embed_dims=(2)
+declare -a lat_embed_dims=(12 8 4 2)
+declare -a bottlenecks=(linear)
 
 # set nn training parameters
-train_batch_size=128  # max regular batch size
-iterations=100001  # number of iterations
-eval_iteration=5000  # eval iterations
+train_batch_size=64  # max regular batch size
+train_iterations=10001  # number of iterations
+valid_iteration=1000 # eval iterations
 
 # set learning rate parameter
-declare -a rates=(0.01)
-learning_rate_steps=5
-
-# set anomaly detection parameter
-algo=hdbscan
-min_cluster_size=100 # the hdbscan min cluster size
-min_samples=1 # the hdbscan min samples
-
-# set hardware parameter
-device=cuda
+declare -a learning_rates=(0.01)
+learning_rate_steps=4
 
 # create logs directory
 mkdir $log_dir
 
 # iterate over bottlenecks
-for bottleneck in "${bottlenecks[@]}"
+for lat_embed_dim in "${lat_embed_dims[@]}"
 do
 
   # iterate over betas
@@ -55,35 +45,41 @@ do
   do
 
     # iterate over seeds
-    for rate in "${rates[@]}"
+    for learning_rate in "${learning_rates[@]}"
     do
 
       # iterate over seeds
-      for seed in "${seeds[@]}"
+      for feat_embed_dim in "${feat_embed_dims[@]}"
       do
 
-        # determine current timestamp
-        timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+        # iterate over seeds
+        for seed in "${seeds[@]}"
+        do
 
-        # create logs directory
-        mkdir $log_dir/$timestamp
+          # determine current timestamp
+          timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
-        # log experiment progress
-        log_timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-        echo "[LOG] $log_timestamp : seed $seed, encoder ${encoder_dim[@]}, decoder ${decoder_dim[@]}, rate $rate experiments started"
+          # create logs directory
+          mkdir $log_dir/$timestamp
 
-        # define experiment postfix
-        exp_postfix=${run_dir}_bt_${bottleneck}
+          # log experiment progress
+          log_timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+          echo "[LOG] $log_timestamp : beta $beta, rate $learning_rate, feat_embed $feat_embed_dim, seed $seed, encoder ${encoder_dim[@]}, decoder ${decoder_dim[@]} experiments started."
 
-        # run experiment
-        echo "nohup python3 main.py -exp_timestamp $timestamp -base_dir $base_dir -data_dir $data_dir -seed $seed -beta $beta -dataset $dataset -sample_eval $sample_eval -sample_size $sample_size -iterations $iterations -eval_iteration $eval_iteration -train_batch_size $train_batch_size -learning_rate $rate -learning_rate_steps $learning_rate_steps -encoder_dim ${encoder_dim[@]} -bottleneck $bottleneck -embed_dim $embed_dim -decoder_dim ${decoder_dim[@]} -device $device -algo $algo -min_cluster_size $min_cluster_size -min_samples $min_samples -exp_postfix ${exp_postfix} > $log_dir/$timestamp/exp_sd_${seed}.log &"
-        nohup python3 main.py -exp_timestamp $timestamp -base_dir $base_dir -data_dir $data_dir -seed $seed -beta $beta -dataset $dataset -sample_eval $sample_eval -sample_size $sample_size -iterations $iterations -eval_iteration $eval_iteration -train_batch_size $train_batch_size -learning_rate $rate -learning_rate_steps $learning_rate_steps -encoder_dim ${encoder_dim[@]} -bottleneck $bottleneck -embed_dim $embed_dim -decoder_dim ${decoder_dim[@]} -device $device -algo $algo -min_cluster_size $min_cluster_size -min_samples $min_samples -exp_postfix ${exp_postfix} > $log_dir/$timestamp/exp_sd_${seed}.log &
+          # define experiment postfix
+          exp_postfix=${run_dir}_sd_${seed}_fe_${feat_embed_dim}_beta_${beta}
 
-        wait
+          # run experiment
+          echo "nohup python3 main.py -exp_timestamp $timestamp -base_dir $base_dir -data_dir $data_dir -seed $seed -beta $beta -dataset $dataset -sample_eval $sample_eval -sample_size $sample_size -train_iterations $train_iterations -valid_iteration $valid_iteration -train_batch_size $train_batch_size -learning_rate $learning_rate -learning_rate_steps $learning_rate_steps -encoder_dim ${encoder_dim[@]} -feat_embed_dim $feat_embed_dim -lat_embed_dim $lat_embed_dim -decoder_dim ${decoder_dim[@]} -exp_postfix ${exp_postfix} > $log_dir/$timestamp/exp_sd_${seed}.log &"
+          # nohup python3 main.py -exp_timestamp $timestamp -base_dir $base_dir -data_dir $data_dir -seed $seed -beta $beta -dataset $dataset -sample_eval $sample_eval -sample_size $sample_size -train_iterations $train_iterations -valid_iteration $valid_iteration -train_batch_size $train_batch_size -learning_rate $learning_rate -learning_rate_steps $learning_rate_steps -encoder_dim ${encoder_dim[@]} -feat_embed_dim $feat_embed_dim -lat_embed_dim $lat_embed_dim -decoder_dim ${decoder_dim[@]} -exp_postfix ${exp_postfix} > $log_dir/$timestamp/exp_sd_${seed}.log &
 
-        # log experiment progress
-        log_timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-        echo "[LOG] $log_timestamp : seed $seed, encoder ${encoder_dim[@]}, decoder ${decoder_dim[@]}, rate $rate experiments completed"
+          wait
+
+          # log experiment progress
+          log_timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+          echo "[LOG] $log_timestamp : beta $beta, rate $learning_rate, feat_embed $feat_embed_dim, seed $seed, encoder ${encoder_dim[@]}, decoder ${decoder_dim[@]} experiments completed."
+
+        done
 
       done
 
