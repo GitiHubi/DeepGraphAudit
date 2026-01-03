ec38f046f113051b3f7794d74ee1271b1e71cad6
Qing Huang <122573136+QingH08@users.noreply.github.com>
2026-01-03 09:54:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ from ExperimentHandler import GraphAutoencoderExperimentDynamic
 def main():
 
     # init client argument parser
-    parser = argparse.ArgumentParser(description='deepApple Experiments')
+    parser = argparse.ArgumentParser(description='deepGNN Experiments')
 
     # general experiment parameter
-    parser.add_argument('-exp_timestamp', help='', nargs='?', type=str,  default=dt.datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S'))
-    parser.add_argument('-exp_series', help='', nargs='?', type=int, default=42)
-    parser.add_argument('-experiment', help='', nargs='?', type=str,  default='graph_autoencoder')
+    parser.add_argument('-exp_timestamp', help='', nargs='?', type=str,  default=dt.datetime.utcnow().strftime('%Y-%m-%d_%M'))
+    parser.add_argument('-exp_series', help='', nargs='?', type=int, default=201)
+    parser.add_argument('-experiment', help='', nargs='?', type=str,  default='graph_autoencoder') #graph_autoencoder, autoencoder
     parser.add_argument('-data_dir', help='', nargs='?', type=str, default='./100_datasets')
     parser.add_argument('-base_dir', help='', nargs='?', type=str, default='./200_experiments')
     parser.add_argument('-exp_postfix', type=str, default='test', help='postfix of experimental runs.')
@@ -47,15 +47,15 @@ def main():
     parser.add_argument('-sample_eval', help='', nargs='?', type=str, default='False')
     parser.add_argument('-sample_size', help='', nargs='?', type=int, default=501)
     parser.add_argument('-min_line_items', help='', nargs='?', type=int, default=2)
-    parser.add_argument('-max_line_items', help='', nargs='?', type=int, default=20)
-    parser.add_argument('-no_global_anomalies', help='', nargs='?', type=int, default=10)
-    parser.add_argument('-no_local_anomalies', help='', nargs='?', type=int, default=10)
-    parser.add_argument('-visualize', type=str, default='True', help='visualize graphs mode')  # static, dynamic
+    parser.add_argument('-max_line_items', help='', nargs='?', type=int, default=1000)
+    parser.add_argument('-no_global_anomalies', help='', nargs='?', type=int, default=100)
+    parser.add_argument('-no_local_anomalies', help='', nargs='?', type=int, default=100)
+    parser.add_argument('-visualize', type=str, default='False', help='visualize graphs mode')  # static, dynamic
 
     # model architecture parameter
-    parser.add_argument('-seed', type=int, default=1111, help='seed value for deterministic results.')
-    parser.add_argument('-encoder_dim', nargs='+', default=[128, 64, 32, 16, 8, 4, 2], help='the dimensions of the encoder gnn layers.')  # [128, 64, 32, 16, 8, 4, 2]
-    parser.add_argument('-decoder_dim', nargs='+', default=[2, 4, 8, 16, 32, 64, 128], help='the dimensions of the decoder fc layers.')  # [2, 4, 8, 16, 32, 64, 128]
+    parser.add_argument('-seed', type=int, default=4444, help='seed value for deterministic results.')
+    parser.add_argument('-encoder_dim', nargs='+', default=[128,64,32,16,8,2], help='the dimensions of the encoder gnn layers.')  # 512,256,128,64,32,16,8,2
+    parser.add_argument('-decoder_dim', nargs='+', default=[2,8,16,32,64,128], help='the dimensions of the decoder fc layers.')  # [2, 4, 8, 16, 32, 64, 128] 2, 8,16,32,64,128,256,512
     parser.add_argument('-encoder_type', type=str, default='embed', help='the type of feature encoding')  # onehot, embed
     parser.add_argument('-encoder_bottleneck', type=str, default='tanh', help='the bottleneck non-linearity.')  # linear, tanh, lrelu
     parser.add_argument('-decoder_bottleneck', type=str, default='sigmoid', help='the bottleneck non-linearity.')  # linear, tanh, lrelu
@@ -63,10 +63,10 @@ def main():
     parser.add_argument('-lat_embed_dim', type=int, default=2, help='the dimension of the graph embeddings.')
 
     # model training parameter
-    parser.add_argument('-train_iterations', type=int, default=501, help='the number of training iterations.')
-    parser.add_argument('-train_batch_size', type=int, default=64, help='the training batch size.')
+    parser.add_argument('-train_iterations', type=int, default=500,help='the number of training iterations.')
+    parser.add_argument('-train_batch_size', type=int, default=256, help='the training batch size.')
     parser.add_argument('-loss', type=str, default='mse', help='the training and validation loss.')  # mse, bce
-    parser.add_argument('-learning_rate', type=float, default=0.0001, help='the learning rate.')
+    parser.add_argument('-learning_rate', type=float, default=0.01, help='the learning rate.')
     parser.add_argument('-learning_rate_steps', type=int, default=3, help='the learning rate steps.')
     parser.add_argument('-weight_decay', nargs='?', type=float, default=1e-6, help='the optimizer weight decay.')
     parser.add_argument('-kl_div_alpha', type=float, default=0.0, help='the kl-divergence loss regularizer.')
@@ -74,7 +74,7 @@ def main():
 
     # model evaluation parameter
     parser.add_argument('-valid_iterations', type=int, default=100, help='the eval training iteration.')
-    parser.add_argument('-valid_batch_size', type=int, default=64, help='the evaluation batch size.')
+    parser.add_argument('-valid_batch_size', type=int, default=256, help='the evaluation batch size.')
     parser.add_argument('-wandb', type=str, default='True', help='enable wandb logging.')
 
     # model grid search parameter
@@ -83,14 +83,15 @@ def main():
     parser.add_argument('-grid_beta', nargs='+', default=[0.5, 0.1], help='the grid search seed.')
 
     # anomaly detection parameter
-    parser.add_argument('-algo', type=str, default='lof', help='the anomaly detection algorithm.') # lof, ocsvm, iforest, knn, hbos
+    parser.add_argument('-algo', type=str, default='iforest', help='the anomaly detection algorithm.') # lof, ocsvm, iforest, hdbscan, knn, hbos
     parser.add_argument('-kernel', type=str, default='rbf', help='the one-class svm kernel.')
     parser.add_argument('-degree', type=int, default=3, help='the one-class svm degree of the polynomial kernel function.')
-    parser.add_argument('-gamma', type=str, default='scale', help='the one-class svm kernel coefficient.') #'scale','auto'
+    parser.add_argument('-gamma', type=str, default='scale', help='the one-class svm kernel coefficient.') #'scale', 'auto'
     parser.add_argument('-n_neighbors', type=int, default=6, help='the number of LOF neighbors.')
     parser.add_argument('-leaf_size', type=int, default=30, help='the leaf size of LOF tree creation.')
     parser.add_argument('-n_neighbors_knn', type=int, default=50, help='the number of knn neighbors.')
     parser.add_argument('-leaf_size_knn', type=int, default=30, help='the leaf size of knn tree creation.')
+    #parser.add_argument('-min_cluster_size', type=int, default=15, help='the mini cluster size for hdbscan.')
 
     # hdbscan grid search parameter
     parser.add_argument('-min_cluster_size', nargs='+', default=50, help='')#[10, 50, 100, 200, 300, 400, 500, 600]
@@ -98,7 +99,8 @@ def main():
     parser.add_argument('-metric', nargs='+', default='euclidean', help='') # ['euclidean', 'manhattan']
     parser.add_argument('-cluster_selection_method', nargs='+', default='eom', help='') #['eom', 'leaf']
 
-    
+
+
     # hdbscan grid search parameter
     parser.add_argument('-grid_min_cluster_size', nargs='+', default=[10, 50, 100, 200, 300, 400, 500, 600], help='')
     parser.add_argument('-grid_min_samples', nargs='+', default=[5, 10, 30, 50, 60, 100], help='')
@@ -171,20 +173,30 @@ def main():
 
             # set journal entry attribute information
             data_parameter['je_header_attributes'] = ['JEIdentifier', 'Source', 'PreparerID']
-            data_parameter['je_segment_attributes_categorical'] = ['AccountType', 'AccountClass', 'GLAccountNumber', 'GLAccountName']
+            data_parameter['je_segment_attributes_categorical'] = ['AccountType', 'AccountClass', 'GLAccountNumber',
+                                                                   'GLAccountName']
             data_parameter['je_segment_attributes_numerical'] = ['Amount']
-            data_parameter['je_attributes'] = data_parameter['je_header_attributes'] + data_parameter['je_segment_attributes_categorical'] + data_parameter['je_segment_attributes_numerical']
+            data_parameter['je_attributes'] = data_parameter['je_header_attributes'] + data_parameter[
+                'je_segment_attributes_categorical'] + data_parameter['je_segment_attributes_numerical']
 
             # set journal entry feature information
             data_parameter['je_header_features'] = ['Y_SOURCE', 'Y_PREPARER_ID']
-            data_parameter['je_segment_features_categorical'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_DEBIT_CREDIT', 'Y_GL_ACCOUNT_TYPE', 'Y_GL_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NUMBER', 'Y_GL_ACCOUNT_NAME']  # Y_JE_ITEM_IDENTIFIER and Y_DEBIT_CREDIT are derived features.
+            data_parameter['je_segment_features_categorical'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_DEBIT_CREDIT',
+                                                                 'Y_GL_ACCOUNT_TYPE', 'Y_GL_ACCOUNT_CLASS',
+                                                                 'Y_GL_ACCOUNT_NUMBER',
+                                                                 'Y_GL_ACCOUNT_NAME']  # Y_JE_ITEM_IDENTIFIER and Y_DEBIT_CREDIT are derived features.
             data_parameter['je_segment_features_numerical'] = ['Y_DMBTR']
-            data_parameter['je_features'] = data_parameter['je_header_features'] + data_parameter['je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
-            data_parameter['je_segment_features'] = data_parameter['je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
+            data_parameter['je_features'] = data_parameter['je_header_features'] + data_parameter[
+                'je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
+            data_parameter['je_segment_features'] = data_parameter['je_segment_features_categorical'] + data_parameter[
+                'je_segment_features_numerical']
 
             # set the visualization features
-            data_parameter['hover_attributes'] = ['Y_JE_IDENTIFIER', 'Y_JE_ITEM_IDENTIFIER', 'Y_GL_ACCOUNT_TYPE', 'Y_GL_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NAME', 'Y_PREPARER_ID', 'Y_SOURCE', 'Y_DMBTR', 'Y_CLASS_NAME']
-            data_parameter['visual_attributes'] = ['Y_BUZEI', 'Y_PREPARER_ID', 'Y_SOURCE', 'Y_ACCOUNT_TYPE', 'Y_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NAME', 'Y_DMBTR']
+            data_parameter['hover_attributes'] = ['Y_JE_IDENTIFIER', 'Y_JE_ITEM_IDENTIFIER', 'Y_GL_ACCOUNT_TYPE',
+                                                  'Y_GL_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NAME', 'Y_PREPARER_ID',
+                                                  'Y_SOURCE', 'Y_DMBTR', 'Y_CLASS_NAME']
+            data_parameter['visual_attributes'] = ['Y_BUZEI', 'Y_PREPARER_ID', 'Y_SOURCE', 'Y_ACCOUNT_TYPE',
+                                                   'Y_ACCOUNT_CLASS', 'Y_GL_ACCOUNT_NAME', 'Y_DMBTR']
 
         # case: sap dataset
         elif experiment_parameter['dataset'] == 'sap':
@@ -204,21 +216,28 @@ def main():
             data_parameter['je_class_name_field'] = 'Y_CLASS_NAME'
 
             # set journal entry attribute information
-            data_parameter['je_header_attributes'] = ['DocumentNr', 'DocType', 'DocTypeDescr', 'UserName Post', 'TransactionDescription']
-            data_parameter['je_segment_attributes_categorical'] = ['PostingKey', 'GL Accountnr', 'GL AccountDescr', 'ProfitCenter', 'CostCenter']
+            data_parameter['je_header_attributes'] = ['DocumentNr', 'DocType', 'UserName Post', 'Transaction'] #delete 'DocTypeDescr'
+            data_parameter['je_segment_attributes_categorical'] = ['PostingKey', 'GL Accountnr', 'GL AccountDescr',
+                                                                   'ProfitCenter' ,'Year','Month']  #delete 'CostCenter'
             data_parameter['je_segment_attributes_numerical'] = ['AmountinUSD']
-            data_parameter['je_attributes'] = data_parameter['je_header_attributes'] + data_parameter['je_segment_attributes_categorical'] + data_parameter['je_segment_attributes_numerical']
+            data_parameter['je_attributes'] = data_parameter['je_header_attributes'] + data_parameter[
+                'je_segment_attributes_categorical'] + data_parameter['je_segment_attributes_numerical']
 
             # set journal entry feature information
             data_parameter['je_header_features'] = ['Y_BLART', 'Y_USNAM', 'Y_TCODE']
-            data_parameter['je_segment_features_categorical'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_DEBIT_CREDIT', 'Y_BSCHL', 'Y_HKONT', 'Y_HKONT_TEXT', 'Y_PRCTR', 'Y_KOSTL']
+            data_parameter['je_segment_features_categorical'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_DEBIT_CREDIT', 'Y_BSCHL',
+                                                                 'Y_HKONT', 'Y_PRCTR', 'Y_YEAR','Y_MONTH','Y_KOSTL', 'Y_HKONT_TEXT'] # delete Y_KOSTL, , 'Y_HKONT_TEXT'
             data_parameter['je_segment_features_numerical'] = ['Y_DMBTR']
-            data_parameter['je_features'] = data_parameter['je_header_features'] + data_parameter['je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
-            data_parameter['je_segment_features'] = data_parameter['je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
+            data_parameter['je_features'] = data_parameter['je_header_features'] + data_parameter[
+                'je_segment_features_categorical'] + data_parameter['je_segment_features_numerical']
+            data_parameter['je_segment_features'] = data_parameter['je_segment_features_categorical'] + data_parameter[
+                'je_segment_features_numerical']
 
             # set the visualization features
-            data_parameter['hover_attributes'] = ['Y_BELNR', 'Y_JE_ITEM_IDENTIFIER', 'Y_BLART', 'Y_USNAM', 'Y_TCODE', 'Y_BSCHL', 'Y_HKONT_TEXT', 'Y_DMBTR', 'Y_PRCTR', 'Y_KOSTL', 'Y_CLASS_NAME']
-            data_parameter['visual_attributes'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_USNAM', 'Y_BLART', 'Y_TCODE', 'Y_BSCHL', 'Y_HKONT_TEXT', 'Y_DMBTR', 'Y_PRCTR', 'Y_KOSTL']
+            data_parameter['hover_attributes'] = [ 'Y_JE_ITEM_IDENTIFIER', 'Y_BLART', 'Y_TCODE',
+                                                  'Y_BSCHL',  'Y_DMBTR', 'Y_CLASS_NAME','Y_KOSTL', 'Y_HKONT_TEXT']   # delete Y_KOSTL, 'Y_HKONT_TEXT',
+            data_parameter['visual_attributes'] = ['Y_JE_ITEM_IDENTIFIER', 'Y_BLART', 'Y_TCODE', 'Y_BSCHL',
+                                                    'Y_DMBTR','Y_KOSTL', 'Y_HKONT_TEXT']  # delete Y_KOSTL, 'Y_HKONT_TEXT',
 
         # case: GNN experiment using full adjacency and feature matrices
         if experiment_parameter['mode'] == 'static':
@@ -271,8 +290,8 @@ def main():
                 # run dynamic AEN experiment
                 _ = exp.run_experiement(parameter=experiment_parameter, data_statistics=data_parameter)
 
+
 # run main function
 if __name__ == '__main__':
-
     # call main function
     main()
